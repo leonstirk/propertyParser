070191d7b24fbae4cc4300107acfe4db248818be
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-01-12 01:00:01 +1300

    auto-commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var json = require('./brxData/companies/10Jan2018.json');
+
+console.log(json)
+
+// db = db.getSiblingDB('test')
+// coll = db.getCollectionNames()
+
+
+// db.test.insertMany([
+//   {
+//     name: "Leon",
+//     age: "26",
+//     status: "alive"
+//   },
+//   {
+//     name: "Kate",
+//     age: "23",
+//     status: "alive"
+//   }
+// ])
+
+// db.test.find().pretty().shellPrint()
---
+++ script.sh
@@ -0,0 +1,3 @@
+# test=$(cat ~/propertyParser/brxData/companies/10Jan2018.json)
+
+mongo 'mongodb+srv://cluster0-gyynp.mongodb.net/test' --authenticationDatabase admin --username leonstirk --password i08wh37Y --quiet script.js
